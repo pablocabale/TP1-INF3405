a8ad5819439cd2c3ff0084d08ff655197d321694
HamzaD0 <hamzadr02@gmail.com>
2023-09-21 19:00:49 -0400

    Envoi de testest

--- Projet Client/TP1/src/ClientHandler.java
+++ Projet Client/TP1/src/ClientHandler.java
@@ -1,9 +1,11 @@
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 public class ClientHandler extends Thread { // pour traiter la demande de chaque client sur un socket particulier
     private Socket socket;
     private int clientNumber;
+
 
     public ClientHandler(Socket socket, int clientNumber) {
         this.socket = socket;
@@ -15,6 +17,13 @@ public class ClientHandler extends Thread { // pour traiter la demande de chaque
         try {
             DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // création de canal d’envoi
             out.writeUTF("Hello from server - you are client#" + clientNumber); // envoi de message
+            while (true){
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+
+                // Attente de la réception d'un message envoyé par le, server sur le canal
+                String helloMessageFromServer = in.readUTF();
+                System.out.println(helloMessageFromServer);
+            }
         }
         catch (IOException e) {
             System.out.println("Error handling client# " + clientNumber + ": " + e);

cf9c3f94f742269f0ae3d42ac726c797298d4a86
raphaelramanitra <raphaelramanitra@gmail.com>
2023-10-16 12:32:16 -0400

    validation class

---
+++ Projet Client/TP1/src/Validation.java
@@ -0,0 +1,15 @@
+public class Validation {
+    public boolean validateIPAddress(String address) {
+        String[] elems = address.split("\\.");
+        if (elems.length != 4) return false;
+        for ( String elem : elems) {
+            int octet = Integer.parseInt(elem);
+            if (octet < 0 || octet > 255) return false;
+        }
+        return true;
+    }
+
+    public boolean validatePort(Integer port){
+        return port >= 0 && port <= 5050;
+    }
+}
